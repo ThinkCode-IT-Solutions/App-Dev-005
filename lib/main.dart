feb35697a3fa9f9a23a4c0d298628ef8474ddab4
Shahzad Farooq <shahzadfarooqdev@gmail.com>
2025-07-07 12:36:14 +0500

    first page

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,25 @@
-// This is the function that runs the program
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main () {
+  runApp(MainApp());
+}
+
+class MainApp extends StatelessWidget{
+  Widget build (BuildContext context){
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: "Save my queen app",
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.lightBlueAccent,
-          title: Text("Facebook"),
-          leading: Icon(Icons.menu),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Icon(Icons.message),
-            ),
-          ],
+          title: Text("New App"),
+          backgroundColor: Colors.red,
+        ),
+        backgroundColor: Colors.grey ,
+        drawer: Drawer(backgroundColor: Colors.blue,),
+        body: Center(
+          child: Text("HY"),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
