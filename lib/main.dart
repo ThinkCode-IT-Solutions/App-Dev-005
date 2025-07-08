a6e0ce872fbea6915b8f280b32d184db3b76571d
Shahzad Farooq <shahzadfarooqdev@gmail.com>
2025-07-08 11:39:53 +0500

    container and column

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,48 @@ class MainApp extends StatelessWidget{
       title: "Save my queen app",
       home: Scaffold(
         appBar: AppBar(
+          centerTitle: true,
           title: Text("New App"),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.teal,
+          leading: Icon(Icons.pets),
+          actions: [
+            Padding(
+              padding:EdgeInsets.all(8) ,
+              child: Icon(Icons.message,color: Colors.black,)
+            ),
+            
+          ],
         ),
         backgroundColor: Colors.grey ,
         drawer: Drawer(backgroundColor: Colors.blue,),
-        body: Center(
-          child: Text("HY"),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+    
+          children: [
+            Text("Helo There"),
+            Icon(Icons.headphones),
+            Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.all(4),
+              width: 200,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.tealAccent,
+                border: Border.all(
+                  color: Colors.black,
+                  width: 3,
+                  style: BorderStyle.solid
+                ),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Center(
+                child: Text("Click Here"),
+              ),
+              
+            )
+          ],
         ),
       ),
     );
